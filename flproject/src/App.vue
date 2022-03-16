41ade9c7b0eaec50ed01c70e82546145b7b0d9cc
AndrewSheu <andrewsheu0617@gmail.com>
2022-03-16 14:15:52 +1000

    create project

---
+++ flproject/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <h1>FI Project</h1>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Hubballi&display=swap')
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: 'Hubballi', cursive;
+}
+
+.container {
+
+}
+
+.btn {
+
+}
+
+
+</style>

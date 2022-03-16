fe246ea3b19dd9fd3ad9c6594ff7710ef8af5cfd
AndrewSheu <andrewsheu0617@gmail.com>
2022-03-16 14:58:16 +1000

    UI

--- flproject/src/App.vue
+++ flproject/src/App.vue
@@ -1,19 +1,35 @@
 <template>
-  <h1>FI Project</h1>
+  <div id="header">
+    <img src="" alt="" />
+    <h1>Marvel soccer Team</h1>
+  </div>
+  <div id="body">
+    <div id="body-top">
+      <h3>Your marvel soccer team</h3>
+    </div>
+    <div id="body-mid">
+      <h1>Search Character</h1>
+      <input id="search-character" />
+    </div>
+    <div id="body-bot">
+      <img src="" alt="" />
+      <h1>Character Name</h1>
+      <h2>Story List</h2>
+      <h2>Story Summary</h2>
+      <button id="pick-char-bot">Pick as soccer team member</button>
+    </div>
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  components: {
-    
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Hubballi&display=swap')
+@import url("https://fonts.googleapis.com/css2?family=Hubballi&display=swap");
 
 * {
   box-sizing: border-box;
@@ -22,16 +38,12 @@ export default {
 }
 
 body {
-  font-family: 'Hubballi', cursive;
+  font-family: "Hubballi", cursive;
 }
 
 .container {
-
 }
 
 .btn {
-
 }
-
-
 </style>

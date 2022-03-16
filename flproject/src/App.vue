87ebac355341eff4bb4a590236f44c8d9d2849a2
AndrewSheu <andrewsheu0617@gmail.com>
2022-03-16 20:22:58 +1000

    body add

--- flproject/src/App.vue
+++ flproject/src/App.vue
@@ -1,32 +1,40 @@
 <template>
-  <div id="header">
-    <img src="" alt="" />
-    <h1>Marvel soccer Team</h1>
-  </div>
-  <div id="body">
-    <div id="body-top">
-      <h3>Your marvel soccer team</h3>
-    </div>
-    <div id="body-mid">
-      <h1>Search Character</h1>
-      <input id="search-character" />
-    </div>
-    <div id="body-bot">
+  <section>
+    <div id="header">
       <img src="" alt="" />
-      <h1>Character Name</h1>
-      <h2>Story List</h2>
-      <h2>Story Summary</h2>
-      <button id="pick-char-bot">Pick as soccer team member</button>
+      <h1>Marvel soccer Team</h1>
     </div>
-  </div>
+    <div id="body">
+      <div id="body-top">
+        <h3>Your marvel soccer team</h3>
+      </div>
+      <div id="body-mid">
+        <h1>Search Character</h1>
+        <input id="search-character" />
+      </div>
+      <div id="body-bot">
+        <img src="" alt="" />
+        <h1>Character Name</h1>
+        <h2>Story List</h2>
+        <h2>Story Summary</h2>
+        <button id="pick-char-bot">Pick as soccer team member</button>
+      </div>
+    </div>
+  </section>
 </template>
 
+
+
 <script>
+
+
+
 export default {
   name: "App",
   components: {},
 };
 </script>
+
 
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Hubballi&display=swap");
@@ -42,6 +50,7 @@ body {
 }
 
 .container {
+  
 }
 
 .btn {

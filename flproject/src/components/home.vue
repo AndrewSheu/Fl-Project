a3fb4c1e4e830dd504d11e040f06c057eabb387d
AndrewSheu <andrewsheu0617@gmail.com>
2022-03-18 16:30:10 +1000

    Router upload

---
+++ flproject/src/components/home.vue
@@ -0,0 +1,33 @@
+<template></template>
+
+<script>
+var md5 = require('md5')
+
+export default {
+    data () {
+        return {
+            collection: 
+            [
+                public_key: '52b5af320809fcc76ffda5df5a0449ec',
+                private_key: '6b1bb422ad3627df36adc13281a8b6016d42bb6' ,
+            ]
+        }
+    }
+    ,
+
+    computed: {
+    hashed () {
+      let concat = this.unixtime + this.collection.public_key + this.collection.private_key
+      return md5(concat)
+    },
+    unixtime () {
+      let datenow = Date.now()
+      var ts = Math.floor(datenow / 1000)
+      return ts
+    }
+  },
+}
+</script>
+
+<style>
+</style>
--- flproject/src/main.js
+++ flproject/src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+
+require('materialize-css/dist/css/materialize.css')
+
+require('materialize-css/dist/js/materialize.js')
+  window.axios = axios
+
 
 createApp(App).mount('#app')
